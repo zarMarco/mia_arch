0f4dfd6f3e4680db5c2a9cbd089bc53dcee13cfa
zarMarco <marco.fioretti83@gmail.com>
2018-11-15 15:37:09 +0100

    modify scripts

--- script/send.sh
+++ script/send.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-btrfs send  /mnt/snaps/root_manj--$(date +%b.%d.%y) | btrfs receive /mnt/backup
-btrfs send /mnt/snaps-home/home_manj--$(date +%b.%d.%y) | btrfs receive /mnt/backup
-btrfs subvolume delete /mnt/snaps/root_manj--$(date +%b.%d.%y)
-btrfs subvolume delete /mnt/snaps-home/home_manj--$(date +%b.%d.%y)
+btrfs send  /mnt/snaps/root_arch--$(date +%b.%d.%y) | btrfs receive /mnt/backup
+btrfs send /mnt/snaps-home/home_arch--$(date +%b.%d.%y) | btrfs receive /mnt/backup
+btrfs subvolume delete /mnt/snaps/root_arch--$(date +%b.%d.%y)
+btrfs subvolume delete /mnt/snaps-home/home_arch--$(date +%b.%d.%y)
